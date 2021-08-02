23e70eefac3aacf609afad5d664a1d63f37d0645
cle1t0 <78626587+cle1t0@users.noreply.github.com>
2021-08-02 17:25:06 -0300

    Update delete-n-rows.py

--- delete-n-rows.py
+++ delete-n-rows.py
@@ -1,21 +1,20 @@
-#Thiago Nishimura de Sousa(SP3040364) e Gabriel Afonso de Brito(Sp3015106)
-#Este script tem como objetivo excluir n linhas da coluna evolucao, tendo como condicao o valor 1
+#This is a simple script to delete a given number of rows from a column in a dataset followed by the creation of a new one without the rows
 
-#Bibliotecas utilizadas
+#Libraries 
 import pandas as pd
 import os
 from pandas import read_excel
 
-#Lendo o arquivo
+#Reading the dataset
 dfs = pd.read_excel('your-data-set',header=0,engine='openpyxl')
 #print(dfs.head)
 
-#Excluindo n quantidade de linhas de acordo com a condição
+#Deteing an given number of rows
 n = 161
-dfn = dfs.drop(dfs[dfs['evolucao'].eq(1)].sample(n).index)
+dfn = dfs.drop(dfs[dfs['the-given-column'].eq(1)].sample(n).index)
 
-#Escrevendo uma nova planilha com dfn
-writer = pd.ExcelWriter('output-planilhav6.xlsx')
+#Creating the new dataset
+writer = pd.ExcelWriter('output-dataset.xlsx')
 dfn.to_excel(writer)
 writer.save()
-print('Nova plhanilha criada!')
+print('New dataset created!')
